01eed780bb4491045ea28417f1ed5037eb9c75c3
Refki Alfarizi <refkialfarizi123@gmail.com>
2026-05-10 22:29:32 +0700

    fix(navbar): Raised from the dead

--- src/components/DashboardLayout.vue
+++ src/components/DashboardLayout.vue
@@ -3,6 +3,7 @@
     <Sidebar />
 
     <div class="flex min-w-0 flex-1 flex-col overflow-hidden">
+    <Header />
       <main class="min-w-0 flex-1 overflow-x-hidden overflow-y-auto bg-slate-50">
         <div class="p-3 sm:p-4 md:p-6 lg:p-8">
           <slot />
@@ -14,4 +15,5 @@
 
 <script setup lang="ts">
 import Sidebar from './AppSidebar.vue'
+import Header from './AppHeader.vue'
 </script>
